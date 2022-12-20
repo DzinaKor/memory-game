7a37900c1abb2a16a9f79b67ff64e9f984b82a0e
DzinaKor <dinakor1922@gmail.com>
2022-12-20 23:59:19 +0400

    feat: add array with cards

--- script.js
+++ script.js
@@ -2,3 +2,16 @@ const cards = document.querySelector('.memory_game');
 const score = document.querySelector('.score');
 const game_score = document.querySelector('.game_score');
 const game_message = document.querySelector('.game_message');
+
+const card_arr = [
+    'bear',
+    'bunny',
+    'cat',
+    'dog',
+    'fox',
+    'lion',
+    'monkey',
+    'owl',
+    'panda',
+    'pig'
+]

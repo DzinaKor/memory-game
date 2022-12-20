1c5ca378b96659f8db11e45a0d3865b2bd29d361
DzinaKor <dinakor1922@gmail.com>
2022-12-20 23:55:45 +0400

    feat: add selectors

---
+++ script.js
@@ -0,0 +1,4 @@
+const cards = document.querySelector('.memory_game');
+const score = document.querySelector('.score');
+const game_score = document.querySelector('.game_score');
+const game_message = document.querySelector('.game_message');
